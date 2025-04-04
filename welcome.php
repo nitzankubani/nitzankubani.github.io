6f8354cda20b600e682d6bc12510cb916740a0a0
nitzankubani <Nitzankubani1@gmail.com>
2025-04-04 10:00:31 +0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,8 +1,11 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+
+
+<?php
+echo "The time is " . date("h:i:sa");
+?>
 
 </body>
 </html>
