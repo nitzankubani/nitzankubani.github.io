4b007c6f8f9f552a56cb886adb154e77dd0ec3e9
nitzankubani <Nitzankubani1@gmail.com>
2025-04-09 22:38:34 +0300

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
 
 function adding(x,y){
-    console.log("my name is nitzan" + "the sum of it is-" + x+ "and" + y);
+    z = x + y;
+    alert ("my name is nitzan " + "the sum of it is-" + x+ "and" + y+ "is: "+ z);
 }
